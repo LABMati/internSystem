edc6580a0c7f251b20025429b5273de91168bc23
Leviosar <joaocampo2102@gmail.com>
2018-05-28 23:56:56 -0300

    .

--- js/home.js
+++ js/home.js
@@ -4,9 +4,16 @@ const showcaseBody = document.querySelectorAll('.showcase-body')
 for (let i = 0; i < showcaseTrigger.length; i++) {
     showcaseTrigger[i].addEventListener("click", ev=>{
         if (showcaseBody[i].style.height == '0vh' ||showcaseBody[i].style.height == '' ) {
+            closeAll(showcaseBody)
             showcaseBody[i].style.height = '20vh'   
         }else{
             showcaseBody[i].style.height = '0vh'
         }
     })
+}
+
+function closeAll(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i].style.height = '0vh'
+    }
 }
