8e872b45df2e52003a3ee9e048acd2c0fdbd0b42
Leviosar <joaocampo2102@gmail.com>
2018-05-29 15:30:42 -0300

    Foi mal

--- pdo.php
+++
@@ -1,5 +0,0 @@
-<?php
-    return new PDO("mysql:host=mysql.hostinger.com.br;dbname=u535468846_lab", "u535468846_boss", "lab124578", [
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-    ]);
-?>
